f50918779fdce031120d77533f907b4d2083516b
Luiz Calaça <luizcalaca@gmail.com>
2022-12-24 15:46:02 -0300

    feat /users/:username/repos done

--- src/controllers/GithubController.ts
+++ src/controllers/GithubController.ts
@@ -44,6 +44,27 @@ class GithubController {
       response.status(500).send(error);
     });
   };
+
+  public getAllUserRepositories = async (request: Request, response: Response) => {
+    const { username } = request.params;
+
+    const options = {
+      hostname: 'api.github.com',
+      path: `/users/${username}/repos`,
+      headers: {
+        'User-Agent': 'luizcalaca',
+        Accept: 'application/vnd.github+json',
+        Authorization: `Authorization: Bearer ${process.env.TOKEN}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    };
+
+    https.get(options, (apiResponse) => {
+      apiResponse.pipe(response);
+    }).on('error', (error) => {
+      response.status(500).send(error);
+    });
+  };
 }
 
 export default GithubController;
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,5 +6,6 @@ const controller = new GithubController();
 
 router.get('/users', controller.getAllUsers);
 router.get('/users/:username/details', controller.getByUser);
+router.get('/users/:username/repos', controller.getAllUserRepositories);
 
 export default router;
