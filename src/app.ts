e38420f8c9995eb9d5150ec228630300df32d8d9
Luiz Calaça <luizcalaca@gmail.com>
2022-12-24 17:11:29 -0300

    feat: adding data caching with apicache middleware

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import apicache from 'apicache';
 import router from './routes/routes';
 
 const app = express();
+const cache = apicache.middleware;
 
 app.use(helmet());
 app.use(express.json());
 app.use(cors());
 app.use(morgan('combined'));
 
+app.use(cache('5 minutes'));
 app.use('/api', router);
 
 export default app;
